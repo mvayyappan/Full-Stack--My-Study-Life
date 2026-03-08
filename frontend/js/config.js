c05e25fcfb191873b8df807a6bc35da7ab51d825
Your Name <your@email.com>
2026-03-08 13:52:42 +0530

    deploy in vercel

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,5 @@
 // Configuration and base URL setup
-const PROD_BACKEND_URL = 'https://full-stack-my-study-life.onrender.com';
+const PROD_BACKEND_URL = 'https://full-stack-my-study-life-1.onrender.com';
 
 const baseUrl = window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost'
   ? 'http://127.0.0.1:8000'
